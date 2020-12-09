bb1085d945de77519f343fccdede762f80a5b80e
Francesco Leardini <piolag@gmail.com>
2020-12-09 12:26:24 +0100

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,7 +31,6 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatButtonModule,
     CommonModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -50,5 +50,4 @@ export class QuizComponent implements OnInit {
     this.answers[questionIndex] = isCorrectAnswer;
     targetForm.disable();
   }
-
 }
